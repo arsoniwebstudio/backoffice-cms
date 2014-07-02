5a2b2ec83ac80caa35c216f9071397755a2396ae
Manuel <manperlop1@gmail.com>
2014-07-02 21:53:17 +0200

    Cambios en configuracion

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,8 +8,9 @@ site.addsitedir('/home/apps/backoffice-cms/env/lib/python2.7/site-packages')
 # Add the app's directory to the PYTHONPATH
 sys.path.append('/home/apps/backoffice-cms/backend')
 sys.path.append('/home/apps/backoffice-cms/backend/config')
+sys.path.append('/home/apps/backoffice-cms/backend/config/settings')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.production'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
 
 # Activate your virtual env
 activate_env=os.path.expanduser("/home/apps/backoffice-cms/env/bin/activate_this.py")
