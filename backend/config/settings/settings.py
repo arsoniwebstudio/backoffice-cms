8f7bea3b357d8fb6ac5742be8b26e2a3148b850f
Manuel <manperlop1@gmail.com>
2014-07-02 21:54:59 +0200

    Cambios en configuracion

--- backend/config/settings/settings.py
+++ backend/config/settings/settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'backend.config.urls'
+ROOT_URLCONF = 'config.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 #WSGI_APPLICATION = 'apps.emulcheese.wsgi.application'
