59fe4e7f6b737e3a26063bb648462d851b785e4a
Manuel <manperlop1@gmail.com>
2014-07-03 02:21:48 +0200

    Cambios en configuracionç

--- backend/manage.py
+++ backend/manage.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management import execute_from_command_line
 sys.path.append('/home/apps/backoffice-cms/backend/config/settings')
 
 if __name__ == "__main__":
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
-
-    from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
